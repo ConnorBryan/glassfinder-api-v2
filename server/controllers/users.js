b4661531798e53c012797395841b667a39b44882
Connor Bryan <cchromium@gmail.com>
2017-11-04 17:01:26 -0500

    Initial stuff for users

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,11 +2,13 @@ const User = require('../models').User;
 
 module.exports = {
   create: async (req, res) => {
+    const { email, emailAgain, password, passwordAgain } = req.body;
+
     try {
-      const user = await User.create({
-        email: req.body.email,
-        password: req.body.password,
-      });
+      validateNewUser(email, emailAgain, password, passwordAgain);
+      await checkUserDoesntAlreadyExist(email);
+
+      const user = await User.create({ email, password });
       
       res.status(200).send(user);
     } catch (e) {
@@ -23,4 +25,21 @@ module.exports = {
       res.status(400).send(e);
     }
   },
+}
+
+/* = = = */
+
+function validateNewUser(email, emailAgain, password, passwordAgain) {
+  if (!compareStrings(email, emailAgain)) throw Error(`Email fields do not match`);
+  if (!compareStrings(password, passwordAgain)) throw Error(`Password fields do not match`);
+}
+
+async function checkUserDoesntAlreadyExist(email) {
+  const user = await User.findOne({ where: { email } });
+
+  if (user) throw Error(`User already exists with email ${email}`);
+}
+
+function compareStrings(stringA, stringB) {
+  return stringA === stringB;
 }
