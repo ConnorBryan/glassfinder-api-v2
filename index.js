b9114f44b5e9eb7651f9a7626f4e9a860b893eea
Connor Bryan <cchromium@gmail.com>
2017-11-12 02:08:37 -0600

    Remove middleware (temporarily)

--- index.js
+++ index.js
@@ -28,35 +28,35 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
   next();
 });
-app.use(async (req, res, next) => {
-  const token = req.body.token || req.query.token || req.headers['x-access-token'];
-  const baseUrl = req.url.split('?')[0];
-  const strippedUrl = baseUrl.split('/').slice(0, 3).join('/');
+// app.use(async (req, res, next) => {
+//   const token = req.body.token || req.query.token || req.headers['x-access-token'];
+//   const baseUrl = req.url.split('?')[0];
+//   const strippedUrl = baseUrl.split('/').slice(0, 3).join('/');
 
-  if (NON_AUTHENTICATED_ROUTES[strippedUrl]) return next();
+//   if (NON_AUTHENTICATED_ROUTES[strippedUrl]) return next();
 
-  if (token) {
-    try {
-      const resolved = await jwt.verify(
-        token,
-        'abc123',
-        (err, decoded) => new Promise((resolve, reject) => err ? reject(err) : resolve(decoded)));
+//   if (token) {
+//     try {
+//       const resolved = await jwt.verify(
+//         token,
+//         'abc123',
+//         (err, decoded) => new Promise((resolve, reject) => err ? reject(err) : resolve(decoded)));
       
-      req.decoded = resolved;
-      next();
-    } catch (e) {
-      res.json({
-        success: false,
-        message: `Unable to verify token`,
-      });
-    }
-  } else {
-    res.json({
-      success: false,
-      message: `Invalid token`,
-    });
-  }
-});
+//       req.decoded = resolved;
+//       next();
+//     } catch (e) {
+//       res.json({
+//         success: false,
+//         message: `Unable to verify token`,
+//       });
+//     }
+//   } else {
+//     res.json({
+//       success: false,
+//       message: `Invalid token`,
+//     });
+//   }
+// });
 
 /*
   R o u t e s
